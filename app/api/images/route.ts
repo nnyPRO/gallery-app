2f5ec85447380b5b50232106f70d308d9377841d
nnyPRO <nanmesanee.17@gmail.com>
2026-03-13 18:24:00 +0700

    fix prisma client import

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { NextRequest, NextResponse } from 'next/server'
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL! })
